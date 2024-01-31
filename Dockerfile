7caa6b20c5b11fb321ba4f5aa642d6ed48026031
Mitchxxx <megboko@gmail.com>
2024-01-31 16:31:19 +0000

    Dockerise a Nodejs App

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:lts-alpine3.17
+
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY package*.json /app/
+
+RUN npm install
+
+COPY . .
+
+EXPOSE  3000
+
+CMD ["node", "server.js"]
+
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+  
+});
+
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname+'/about.html'));
+   
+  });
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
