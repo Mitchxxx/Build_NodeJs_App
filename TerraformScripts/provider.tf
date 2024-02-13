fee26a954b9303cd90464799d3d84a99ee0e315f
Mitchxxx <megboko@gmail.com>
2024-02-13 15:22:55 +0000

    Added terraform backend block

--- TerraformScripts/provider.tf
+++ TerraformScripts/provider.tf
@@ -1,21 +1,21 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-#terraform {
-#  required_providers {
-#    azurerm = {
-#      source  = "hashicorp/azurerm"
-#      version = "=3.0.0"
-#    }
+terraform {
+ required_providers {
+   azurerm = {
+     source  = "hashicorp/azurerm"
+     version = "~> 3.0.0"
+   }
 
-#  }
-#backend "azurerm" {
-#      resource_group_name  = "remote"
-#      storage_account_name = "boboguguruyole"
-#      container_name       = "containertfstate"
-#      key                  = "terraform.tfstate"
-#  }
+ }
+backend "azurerm" {
+     resource_group_name  = "remote"
+     storage_account_name = "mitchyxxx16986"
+     container_name       = "containertfstate"
+     key                  = "terraform.tfstate"
+ }
 
-#}
+}
 
 # Configure the Microsoft Azure Provider
 
